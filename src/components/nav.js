95c1b8866ad697fa22200bc88f5feb38cdcdf221
Lspacedev <145987706+Lspacedev@users.noreply.github.com>
2024-07-12 13:46:22 +0200

    added react files

---
+++ src/components/nav.js
@@ -0,0 +1,19 @@
+function Nav(props) {
+  return (
+    <div className="nav">
+      <div className="logo">
+        <span>Lyf-&#41;</span> /isurance@tiropay.au
+      </div>
+      <div className="nav-buttons-menu">
+        <button className="log-in">Log In</button>
+        <button className="start">Get Started</button>
+        <div className="burger">
+          <div></div>
+          <div></div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Nav;
